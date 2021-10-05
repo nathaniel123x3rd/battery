529813ecb71fbfc172d2a91a506b81e3e2210ca9
Nathanielx3rd <81175228+nathaniel123x3rd@users.noreply.github.com>
2021-10-05 11:32:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const percentage = document.querySelector('.percentage');
+const percent = document.querySelector('.percent');
+navigator.getBattery().then(function(battery){
+    percentage.style.width = battery.level * 100 + '%';
+    percent.innerHTML = battery.level * 100 + '%';
+})
+
+const sec = document.querySelector('.sec');
+const toggle = document.querySelector('.toggle');
+toggle.onclick = function(){
+    sec.classList.toggle('dark');
+}
