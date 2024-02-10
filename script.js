45ae2cfc822092f67d56d817a33822b2084ef56e
Nathanielx3rd <81175228+nathaniel123x3rd@users.noreply.github.com>
2024-02-10 11:46:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,5 +8,5 @@ navigator.getBattery().then(function(battery){
 const sec = document.querySelector('.sec');
 const toggle = document.querySelector('.toggle');
 toggle.onclick = function(){
-    sec.classList.toggle('dark container');
+    sec.classList.toggle('dark');
 }
